fc395c3622876fe38ec0fbbc50150dc47413f718
Andrew C <andrewclark3942@gmail.com>
2022-03-13 14:26:57 -0400

    Finished state of lab 7
    Error, warning, and info messages print with a correct date and time stamp

--- src/server/data_storage.adb
+++ src/server/data_storage.adb
@@ -9,10 +9,10 @@
 ---------------------------------------------------------------------------
 
 -- TODO: Remove dependency on Ada.Text_IO. Use a real logger.
-with Ada.Text_IO;
+with Server_Logger;
 with Postgresql;
 
-use Ada.Text_IO;
+use Server_Logger;
 use Postgresql;
 
 
@@ -22,14 +22,14 @@ package body Data_Storage is
    begin
       -- TODO: Database connectivity information should come from a configuration file.
       Postgresql.Connect("localhost", 5432, "ThumperServer", "thumper", "rabbitsfoot");
-      Put_Line("Connected to the Database.");
+      Write_Information("Connected to the Database.");
    end Initialize;
 
 
    procedure Shutdown is
    begin
       Postgresql.Disconnect;
-      Put_Line("Disconnected From the Database");
+      Write_Information("Disconnected From the Database");
    end Shutdown;
 
 
--- src/server/server_logger.adb
+++ src/server/server_logger.adb
@@ -10,12 +10,57 @@
 pragma SPARK_Mode(Off);
 
 with Ada.Text_IO;
+with Ada.Calendar;
+use Ada.Calendar;
+with Ada.Strings.Fixed;
+use Ada.Strings.Fixed;
+with Ada.Strings;
+use Ada.Strings;
+
 
 package body Server_Logger is
 
+   function Format_Timestamp return String is
+   now : Time;
+   now_String : String(1..19) := (others => ' ');
+   now_Year : Integer;
+   now_Month : Integer;
+   now_Day : Integer;
+   now_Second_Duration : Day_Duration;
+   now_Second : Positive;
+   now_Hour : Positive;
+   now_Minute : Positive;
+   now_Seconds : Positive;
+   begin
+      now := Clock;
+      Ada.Calendar.Split(Date => now, Year => now_Year, Month => now_Month, Day => now_Day, Seconds => now_Second_Duration);
+      now_Second := Integer(now_Second_Duration);
+      now_Hour := now_Second/3600;
+      now_Minute := (now_Second - (now_Hour * 3600)) / 60;
+      now_Seconds := now_second - (now_Hour * 3600) - (now_Minute * 60);
+      --now_String :=;
+      now_String := Trim(Integer'Image(now_Year),Both) & "-" & 
+      Trim((if now_Month < 10 then "0" & Trim(Integer'Image(now_Month),Both) else Integer'Image(now_Month)),Both) & "-" & 
+      Trim((if now_Day < 10 then "0" & Trim(Integer'Image(now_Day),Both) else Integer'Image(now_Day)),Both) & " " & 
+      Trim((if now_Hour < 10 then "0" & Trim(Integer'Image(now_Hour),Both) else Integer'Image(now_Hour)),Both) & ":" & 
+      Trim((if now_Minute < 10 then "0" & Trim(Integer'Image(now_Minute),Both) else Integer'Image(now_Minute)),Both) & ":" & 
+      Trim((if now_Seconds < 10 then "0" & Trim(Integer'Image(now_Seconds),Both) else Integer'Image(now_Seconds)),Both);
+      return now_String;
+   end Format_Timestamp;
+
    procedure Write_Error(Message : in String) is
    begin
-      Ada.Text_IO.Put_Line("*** ERROR: " & Message);
+      Ada.Text_IO.Put_Line("*** ERROR: " & Format_Timestamp & " " & Message);
    end Write_Error;
+
+   procedure Write_Information(Message: in String) is
+   begin
+      Ada.Text_IO.Put_Line ("*** Info: " & Format_Timestamp & " " & Message);
+   end Write_Information;
+
+   procedure Write_Warning(Message : in String) is
+   begin
+      Ada.Text_IO.Put_Line("*** Warning: " & Format_Timestamp & " " & Message);
+   end Write_Warning;
 
 end Server_Logger;
